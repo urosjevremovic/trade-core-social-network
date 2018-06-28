04254c3d8510b35310664c0b8b03d02360670e11
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-06-28 15:33:38 +0200

    added post save signal for handling clearbit enrichment that provides additional data about user on signup

--- user/utils.py
+++ user/utils.py
@@ -2,12 +2,15 @@ from urllib.parse import quote
 
 import requests
 import json
+import clearbit
 
 
 with open('api_keys.json') as f:
     api_keys = json.load(f)
 
 never_bounce_api_key = api_keys['never_bounce_api_key']
+clearbit.key = api_keys['clearbit_api_key']
+print(clearbit.key)
 
 
 def check_mail_validity_with_never_bounce(mail):
@@ -19,3 +22,9 @@ def check_mail_validity_with_never_bounce(mail):
     except KeyError:
         print('Reached maximum daily number of email verification checks.')
         pass
+
+
+def get_person_detail_based_on_provided_email(mail):
+    person = clearbit.Enrichment.find(email=mail, stream=True)
+    if person is not None:
+        return person
