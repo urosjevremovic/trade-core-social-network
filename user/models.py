ce1189e9d14ac051d94b9e29cf61af165ca7119b
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-06-29 13:14:08 +0200

    switched logic for getting additional info about user with clearbit api back to post save signal

--- user/models.py
+++ user/models.py
@@ -1,7 +1,14 @@
+from django.core.files.base import ContentFile
 from django.core.validators import validate_email
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.text import slugify
 
+from urllib import request as request_lib
+
+from user.utils import get_person_detail_based_on_provided_email
 from user.validators import custom_validate_email
 
 
@@ -13,3 +20,20 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        user_data = get_person_detail_based_on_provided_email(instance.email)
+        try:
+            instance.first_name = user_data['person']['name']['givenName']
+            instance.last_name = user_data['person']['name']['familyName']
+        except TypeError:
+            pass
+        try:
+            photo_url = user_data['person']['avatar']
+            response = request_lib.urlopen(photo_url)
+            image_name = '{}.jpg'.format(slugify(instance.username))
+            instance.photo.save(image_name, ContentFile(response.read()))
+        except (TypeError, AttributeError):
+            pass
--- user/serializers.py
+++ user/serializers.py
@@ -1,11 +1,6 @@
-from django.core.files.base import ContentFile
-from django.utils.text import slugify
 from rest_framework import serializers
 
 from user.models import User
-from user.utils import get_person_detail_based_on_provided_email
-
-from urllib import request as request_lib
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -29,19 +24,6 @@ class UserSerializer(serializers.ModelSerializer):
         )
 
         user.set_password(validated_data['password'])
-        user_data = get_person_detail_based_on_provided_email(user.email)
-        try:
-            user.first_name = user_data['person']['name']['givenName']
-            user.last_name = user_data['person']['name']['familyName']
-        except TypeError:
-            pass
-        try:
-            photo_url = user_data['person']['avatar']
-            response = request_lib.urlopen(photo_url)
-            image_name = '{}.jpg'.format(slugify(user.username))
-            user.photo.save(image_name, ContentFile(response.read()))
-        except (TypeError, AttributeError):
-            pass
         user.save()
         return user
 
